ced41e8ffcfd7336a229a3f9aed7cc3c023dab45
MalinduPanchala <malindupanchala@gmail.com>
2021-06-07 21:46:17 +0530

    server.js changes

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-//hello
+//--------------------------node modules--------------------------------//
+
+const express = require("express");
+const cors = require("cors");
+const mongo = require("mongoose");
+const config = require("config");
+
+//-----------------------------------------------------------------//
